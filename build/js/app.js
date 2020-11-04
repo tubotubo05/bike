443305b6d12fc390e3f5d095c469f387ef03f343
tubotubo05 <69897229+tubotubo05@users.noreply.github.com>
2020-11-04 18:33:02 +0900

    animation

--- build/js/app.js
+++ build/js/app.js
@@ -5,7 +5,38 @@ c.height = 350;
 
 document.body.appendChild(c);
 
+var perm = [];
+
+while (perm.length < 255) {
+  while (perm.includes(val = Math.floor(Math.random() * 255)));
+  perm.push(val);
+}
+var lerp = (a, b, c) => a + (b - a) * (1 - Math.cos(t * Math.PI)) / 2;
+
+var noise = x => {
+  x = x * 0.01 % 255;
+  return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
+}
+
+var t = 0;
 function loop() {
+
+  t += 1;
   ctx.fillStyle = "#19f";
   ctx.fillRect(0, 0, c.with, c.height);
+
+  ctx.fillStyle = "black";
+
+  ctx.beginPath();
+  ctx.moveTo(0, c.height);
+
+
+  for (var i = 0; i < c.width; i++) {
+    ctx.lineTo(i, c.height - noise(t + i) * 0.25);
+  }
+
+  ctx.lineTo(c.width, c.height);
+
+  ctx.fill();
+  requestAnimationFrame(loop);
 }
